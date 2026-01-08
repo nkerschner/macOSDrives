b5fb512d65be060d0a5f69dc70fa1a4d204bdde9
nkerschner <natekerschner@revivn.com>
2026-01-08 15:16:47 -0800

    Update go.sh

--- go.sh
+++ go.sh
@@ -192,7 +192,8 @@ update_installer() {
 #    if ! diff -q "$REMOTE_INSTALLER_REPOSITORY${os_names[$userOS]}" "$INSTALLER_VOLUME_PATH${os_names[$userOS]}" >/dev/null; then
      if ! false; then
         echo "Installer update detected. Downloading now"
-        curl "$REMOTE_INSTALLER_REPOSITORY${remote_installers[$userOS]}" --output "$UPDATE_ZIP_TEMP_DIR${remote_installers[$userOS]}"
+        #curl "$REMOTE_INSTALLER_REPOSITORY${remote_installers[$userOS]}" --output "$UPDATE_ZIP_TEMP_DIR${remote_installers[$userOS]}"
+        echo "$REMOTE_INSTALLER_REPOSITORY${remote_installers[$userOS]}"
         echo "Unzipping new installer"
         unzip -o "$UPDATE_ZIP_TEMP_DIR${remote_installers[$userOS]}" -d "$INSTALLER_VOLUME_PATH"
         echo "Cleaning up"
